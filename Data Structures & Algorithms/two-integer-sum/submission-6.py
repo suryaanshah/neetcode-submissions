74c040281e139865d82bb824787f732f88f5d12c
Suryaansh Chawla <suryaanshchawla@tutamail.com>
2026-05-10 13:52:36 +0530

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for n in nums:
+            m = target - n
+            if m in nums and nums.index(n)!=nums.index(m): 
+                return [nums.index(n), nums.index(m)]
+            elif nums.index(n)==nums.index(m): 
+                i = nums.index(n)
+                nums.remove(n)
+                if m in nums:
+                    nums.insert(i, n)
+                    return [i, nums.index(m)]
+            else: 
+                pass
