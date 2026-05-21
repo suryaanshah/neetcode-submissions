7091d81f1f0ff36f7a5e08767b21d2d017bcbb92
Suryaansh Chawla <suryaanshchawla@gmail.com>
2026-05-21 19:46:50 +0530

    Add: majority-element - submission-3

---
+++ Data Structures & Algorithms/majority-element/submission-3.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        // moore voting algo (maj element will have largest freq so subtacting all other freq (<n/2) will still give positive & greatest sum)
+        int res = nums[0];
+        int count = 0;
+        for (int val : nums) {
+            if (count == 0) {res = val;}
+            else if (res == val) {count++;}
+            else {count--;}
+        }
+        return res;
+    }
+};
